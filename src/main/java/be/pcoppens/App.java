844dd6e8bdacdd236210590961d49fd2b9c6413c
AC\cppptr <patrice.coppens@forem.be>
2018-05-31 09:20:34 +0200

    maven project template

---
+++ src/main/java/be/pcoppens/App.java
@@ -0,0 +1,13 @@
+package be.pcoppens;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
